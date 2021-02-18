06452c05ff11d340ccea6067ca9ba76b6102074c
Rahan <wangleibniz01@gmail.com>
2021-02-18 17:38:39 +0800

    wrapping up and  minor changes

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ class DrumKit {
     this.isPlaying = null
     this.selects = document.querySelectorAll("select")
     this.muteBtns = document.querySelectorAll(".mute")
+
+    this.tempoSlider = document.querySelector(".tempo-slider")
   }
   activePad() {
     this.classList.toggle("active")
@@ -110,6 +112,24 @@ class DrumKit {
       }
     }
   }
+
+  changeTempo(e) {
+    const tempoText = document.querySelector(".tempo-nr")
+
+    tempoText.innerText = e.target.value
+  }
+
+  updateTempo() {
+    this.bpm = e.target.value
+    clearInterval(this.isPlaying)
+    this.isPlaying = null
+
+    const playBtn = document.querySelector(".play")
+
+    if (playBtn.classList.contains("active")) {
+      this.start()
+    }
+  }
 }
 
 const drumKit = new DrumKit()
@@ -138,4 +158,12 @@ drumKit.muteBtns.forEach((btn) => {
   btn.addEventListener("click", (e) => {
     drumKit.mute(e)
   })
+})
+
+drumKit.tempoSlider.addEventListener("input", (e) => {
+  drumKit.changeTempo(e)
+})
+
+drumKit.tempoSlider.addEventListener("change", (e) => {
+  drumKit.updateTempo(e)
 })
